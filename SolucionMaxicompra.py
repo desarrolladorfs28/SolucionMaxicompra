1ce82945787375e4fa1ce75adca3a7c308cbec96
Edson <edson.huidobro@gmail.com>
2026-05-18 02:59:12 +0000

    Con la finalidad de considerar arreglos, he agregado una fórmula para que el cálculo del pago semanal considere los días hábiles del mes. Además he modificado el orden de la pregunta sobre planilla u honorarios, con la finalidad de coincide con el diagrama de flujo establecido inicialmente

--- SolucionMaxicompra.py
+++ SolucionMaxicompra.py
@@ -1,27 +1,32 @@
+
 # 1. Estructura repetitiva de reinicio (para permitir múltiples consultas)
 continuar_programa = "si"
 while continuar_programa.lower() == "si":
     
+    
     # Datos de entrada
     nombre = input("Nombre del trabajador: ")
-    tipo_contrato = input("Tipo de contrato (planilla/honorarios): ").lower()
     monto_mensual = float(input(f"Sueldo/Pago mensual en S/: "))
+    mes = input("El mes de cálculo es: ").lower()
     
-    asignacion_familiar = 0
-    if tipo_contrato == "planilla":
-        tiene_asig = input("¿Tiene asignación familiar? (si/no): ").lower()
-        if tiene_asig == "si":
-            # S/ 102.50 mensual / 4 semanas = 25.625
-            asignacion_familiar = 25.625
-
-    # Primera Etapa – Registro de la Semana
+    #Definimos los arreglos para los días hábiles por meses
+    dias_habiles = 0
+    nombre_meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"]
+    dias_laborables = [21,20,22,20,20,21,20,20,22,21,21,20]
+    for i in range (0,len(nombre_meses),1):
+        if mes == nombre_meses [i]:
+            dias_habiles = dias_laborables [i]
+            
+    
+        # Primera Etapa – Registro de la Semana
     especial = input("¿Tuvo vacaciones, ausentismo, feriados o licencias? (si/no): ").lower()
     
     if especial == "si":
         print("Debe dirigirse a Recursos Humanos para el procesamiento de su pago.")
     else:
         total_horas_extras = 0
-        pago_base_semanal = monto_mensual / 4 
+        #1. El cálculo del pago semanal se estima multiplicando los 5 días trabajadores de la semana normal entre los días hábiles totales del mes.
+        pago_base_semanal = monto_mensual * 5/dias_habiles #
         jornada_incompleta = False
 
         # 2. Estructura repetitiva principal (Iteración de los 5 días)
@@ -43,9 +48,21 @@ while continuar_programa.lower() == "si":
                 break # El proceso se detiene por no cumplir jornada
             elif horas > 8:
                 total_horas_extras += (horas - 8)
-
+    
+    
+         # Segunda etapa: Cálculo del pago. 
         if not jornada_incompleta:
-            # Segunda etapa: Cálculo del pago
+            tipo_contrato = input("Tipo de contrato (planilla/honorarios): ").lower()
+            
+            #De ser planilla, se valida si se tiene asignación familiar.
+            asignacion_familiar = 0
+            if tipo_contrato == "planilla":
+                tiene_asig = input("¿Tiene asignación familiar? (si/no): ").lower()
+                if tiene_asig == "si":
+            # S/ 113 mensual * 5 / número de días hábiles para estimar el pago exacto de manera semanal.
+                    asignacion_familiar = 113*5/dias_habiles
+                    
+            #Para calcular el valor de la hora extra, se utiliza el cálculo de dividir el monto mensual entre el factor estándar de 30 días x 8 horas diarias.
             valor_hora_normal = (monto_mensual / 30) / 8
             monto_horas_extras = total_horas_extras * (valor_hora_normal * 2)
 
@@ -75,7 +92,7 @@ while continuar_programa.lower() == "si":
                 remun_bruta = pago_base_semanal + monto_horas_extras
                 
                 retencion_cuarta = 0
-                if remun_bruta > 1500:
+                if monto_mensual > 1500:
                     retencion_cuarta = remun_bruta * 0.08
                 
                 remun_neta = remun_bruta - retencion_cuarta
